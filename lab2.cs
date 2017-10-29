a20e6180f1dee095f25b5aab3c71100af9a3627b
Beniraj Rajanish <beni.rajanish@gmail.com>
2017-10-29 16:55:52 -0400

    first commit

---
+++ lab2.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int num1 = 0;
+            string check_cont1 = "Y";
+            string check_cont2 = "N";
+
+
+            while (check_cont1 == "Y")
+
+            {
+                Console.WriteLine("Enter an integer between 1 and 100 :");
+                input = Console.ReadLine();
+
+
+                bool success = int.TryParse(input, out num1);
+
+                if (success == false)
+                {
+                    Console.WriteLine("It's not an integer,Please enter an integer :");
+
+                }
+
+                if (num1 % 2 == 1)
+
+                {
+                    Console.WriteLine(num1 + " and  Odd");
+                }
+
+                else if (num1 >= 2 && num1 <= 25)
+                {
+                    Console.WriteLine("Even and less than 25");
+                }
+
+                else if (num1 >= 26 && num1 <= 60)
+
+                {
+                    Console.WriteLine("Even");
+                }
+                else if (num1 > 60)
+                {
+
+                    Console.WriteLine(num1 + " and  Even");
+                }
+
+                Console.WriteLine("Do you like to continue (Y/N) : ");
+                check_cont1 = Console.ReadLine();
+            }
+        
+                Console.WriteLine("Bye!");
+              check_cont2 = Console.ReadLine();
+            
+        }
+    }
+}
---
+++ lab3.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int digit;
+            string check_cont1 = "Y";
+            string check_cont2 = "N";
+            double square;
+            double cube;
+
+            while (check_cont1 == "Y")
+            {
+
+                Console.WriteLine("Enter an integer :");
+                input = Console.ReadLine();
+
+
+                bool success = int.TryParse(input, out digit);
+
+                if (success == false)
+                {
+                    Console.WriteLine("It's not an integer,Please enter an integer :");
+                    Console.WriteLine("Do you like to continue (Y/N) :");
+
+                    check_cont1 = Console.ReadLine();
+                }
+
+
+                for (int i = 1; i <= digit; i++)
+                {
+
+                    square = Math.Pow(i, 2);
+                    cube = Math.Pow(i, 3);
+                    Console.WriteLine(i + "       " + square + "        " + cube);
+                }
+                Console.ReadLine();
+            }
+
+                Console.WriteLine("Bye!");
+                check_cont2 = Console.ReadLine();
+            
+        }
+    }
+}
